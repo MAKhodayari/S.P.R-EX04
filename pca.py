db07b79312c62d7993504a02d3e490363dee1b62
Mohammad Ali Khodayari <makhodayari99@gmail.com>
2022-12-20 21:46:50 +0330

    PCA

---
+++ pca.py
@@ -0,0 +1,44 @@
+import numpy as np
+import utilities as utl
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    # Preparing Data
+    data, x, y = utl.load_dataset()
+
+    feat_mean, data = utl.zero_mean(data)
+
+    vectors = utl.pca(data)
+
+    # PCA
+    dims = [2, 3]
+    pca_fig = plt.figure()
+    pca_fig.suptitle('PCA For Olivetti Faces')
+    for i, dim in enumerate(dims):
+        vector = vectors[:dim]
+        proj = utl.project(data, vector)
+        if dim != 3:
+            pca_ax = pca_fig.add_subplot(1, 2, i + 1)
+            pca_ax.scatter(proj[:, 0], proj[:, 1], c=y)
+        else:
+            pca_ax = pca_fig.add_subplot(1, 2, i + 1, projection='3d')
+            pca_ax.scatter(proj[:, 0], proj[:, 1], proj[:, 2], c=y)
+            pca_ax.set_zlabel('PC-3')
+        pca_ax.set_title('Faces With ' + str(dim) + ' Principal Components')
+        pca_ax.set(xlabel='PC-1', ylabel='PC-2')
+    pca_fig.tight_layout()
+
+    # Reconstruction
+    components = [1, 20, 50, 150]
+    recon_fig = plt.figure()
+    recon_fig.suptitle('Reconstruct From PCA')
+    for i, component in enumerate(components):
+        vector = vectors[:component]
+        reconstruct = np.dot(data, np.dot(vector.T, vector)) + feat_mean
+        recon_ax = recon_fig.add_subplot(2, 2, i + 1)
+        recon_ax.imshow(reconstruct[20].reshape(64, 64))
+        recon_ax.set_title(f'K = {component}')
+    recon_fig.tight_layout()
+
+    plt.show()
--- utilities.py
+++ utilities.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 
 def load_dataset():
     olivetti = datasets.fetch_olivetti_faces()
+    data = olivetti.data
     x = olivetti.images
     y = olivetti.target
-    x = x.reshape((400, 4096))
-    return x, y
+    # x = x.reshape((400, 4096))
+    return data, x, y
 
 
 def calc_mu(x, y):
@@ -21,15 +22,15 @@ def calc_mu(x, y):
     return mu
 
 
-def cal_sw_sb(x,y):
+def cal_sw_sb(x, y):
     _, n_feature = x.shape
     classes, counts = np.unique(y, return_counts=True)
-    S_W = np.zeros((n_feature,n_feature))
-    S_B = np.zeros((n_feature,n_feature))
+    S_W = np.zeros((n_feature, n_feature))
+    S_B = np.zeros((n_feature, n_feature))
     mu = calc_mu(x, y)
     mean_overall = np.mean(x, axis=0)
     for cl in classes:                 
-        m_s = (x[y == cl]-mu[cl])
+        m_s = (x[y == cl] - mu[cl])
         S_W += np.dot(m_s.T, m_s)
     for cl in classes:
         mean_diff = (mu[cl] - mean_overall).reshape(n_feature, 1)
@@ -45,3 +46,28 @@ def LDA(X, y):
     eigenvalues = eigenvalues[index]
     eigenvectors = eigenvectors[index]
     return eigenvectors
+
+
+def zero_mean(data):
+    feature_means = np.mean(data, axis=0)
+    zero_mean_data = data - feature_means
+    return feature_means, zero_mean_data
+
+
+def calc_eigen_params(data):
+    cov = np.dot(data.T, data)
+    eigen_values, eigen_vectors = np.linalg.eig(cov)
+    return eigen_values, eigen_vectors
+
+
+def pca(data):
+    eigen_values, eigen_vectors = calc_eigen_params(data)
+    index = np.argsort(eigen_values)[::-1]
+    eigen_vectors = eigen_vectors[index]
+    return eigen_vectors
+
+
+def project(data, vector):
+    projection = np.dot(data, vector.T)
+    return projection
+
